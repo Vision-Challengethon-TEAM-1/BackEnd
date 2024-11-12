2f13c55aed2d305ae806b7b806563bb64dd7132b
lsmin3388 <lsmin3388_@naver.com>
2024-11-12 23:27:19 +0900

    Chore: build.gradle에 KotlinDSL 적용 및 Security 관련 의존성 추가

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+	id("java")
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.vision-hackathon"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(17))
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven(url = "https://repo.spring.io/milestone")
+}
+
+extra["springAiVersion"] = "1.0.0-M3"
+
+dependencies {
+	// Spring Boot Starters
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// Spring AI
+	implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
+	developmentOnly("org.springframework.ai:spring-ai-spring-boot-docker-compose")
+
+	// Security
+	implementation("org.springframework.security:spring-security-oauth2-client")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+	// Database
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+	// Docker Compose
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	// Testing
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+		mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.5")
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+tasks.named<Test>("test") {
+	useJUnitPlatform()
+}
